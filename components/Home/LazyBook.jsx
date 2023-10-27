923d53eada7cc016b7d07e2dad3ae8e4f90c7251
Mohammad <m789219@gmail.com>
2023-10-27 15:45:20 +0330

    change loading design

--- components/Home/LazyBook.jsx
+++ components/Home/LazyBook.jsx
@@ -1,7 +1,7 @@
 export default function LazyBookComponent() {
   return (
-    <div className="flex justify-center items-center mt-[35%] ">
-    <div className="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-green-500 via-blue-500 to-red-500 ">
+    <div className="flex justify-center items-center sm:mt-[15%] ">
+    <div className="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-green-500 via-blue-500  ">
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-gray-200 rounded-full border-2 border-white"></div>
     </div>
 </div>
